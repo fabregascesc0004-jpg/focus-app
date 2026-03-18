935ab4550badcad49b02b143a2e22c312b237c17
fabregascesc0004-jpg <fabregascesc0004@gmail.com>
2026-03-19 08:22:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// FOCUS Service Worker v10
+// FOCUS Service Worker v11
 // オフライン対応：HTMLとアイコンをキャッシュする
 
-var CACHE_NAME = 'focus-v10';
+var CACHE_NAME = 'focus-v11';
 var CACHE_URLS = [
   '/',
   '/index.html',
